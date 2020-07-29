70ff033ba0abea001217437dcb562d05821b38bb
Dexyod <dexyod43@gmail.com>
2020-07-29 17:52:09 +0000

    added movement functionality to input.js

--- input.js
+++ input.js
@@ -1,8 +1,11 @@
+// Stores the active TCP connection object.
+let connection;
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,12 +15,21 @@ const setupInput = function () {
 };
 
 const handleUserInput = (key) => {
-  (key) => {
-    if (key === "\u0003") {
-      process.exit();
-    }
-    process.stdout.write(key);
-  };
+  if (key === "\u0003") {
+    process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
 };
 
 module.exports = { setupInput };
